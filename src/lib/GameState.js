269c566e799fa3f3a127f87ac86ca1607584a4e2
kamweru <subtillia@gmail.com>
2024-09-26 21:35:43 +0300

    code improvements

--- src/lib/GameState.js
+++ src/lib/GameState.js
@@ -9,7 +9,7 @@ export class GameState {
     this.boxes = [];
     this.edges = {};
     this.players = {
-      current: 0,
+      current: 1,
       moves: {
         0: [],
         1: [],
@@ -220,112 +220,264 @@ export class GameState {
   }
 
   setEdge(edge) {
-    this.edges[edge.rowColKey].drawn = true;
-    this.edges[edge.rowColKey].from.isOccupied = true;
-    this.edges[edge.rowColKey].to.isOccupied = true;
-    for (let boxIndex in this.boxes) {
-      if (this.boxes[boxIndex].edges[edge.rowColKey]) {
-        this.boxes[boxIndex].edges[edge.rowColKey] = this.edges[edge.rowColKey];
-        this.boxes[boxIndex].drawnEdgesCount = Object.values(
-          this.boxes[boxIndex].edges
-        ).reduce((acc, curr) => {
-          if (curr.drawn) {
-            acc += 1;
-          }
-          return acc;
-        }, 0);
-        if (this.boxes[boxIndex].drawnEdgesCount === 4) {
-          this.boxes[boxIndex].complete = true;
-        }
+    if (this.edges[edge.rowColKey].drawn) {
+      return;
+    }
+    let edgeBoxes = this.boxes.filter(
+        (b, i) =>
+          !b.complete &&
+          b.edges[edge.rowColKey] &&
+          !b.edges[edge.rowColKey].drawn &&
+          b.edges[edge.rowColKey].boxIndex === i
+      ),
+      completedBoxes = 0;
+    for (let edgeBox of edgeBoxes) {
+      this.edges[edge.rowColKey].from.isOccupied = true;
+      this.edges[edge.rowColKey].to.isOccupied = true;
+      this.edges[edge.rowColKey].drawn = true;
+      this.edges[edge.rowColKey].c =
+        this.players.current === 0 ? "red" : "green";
+      this.boxes[edgeBox.id].edges[edge.rowColKey] = this.edges[edge.rowColKey];
+      this.boxes[edgeBox.id].drawnEdgesCount++;
+      if (this.boxes[edgeBox.id].drawnEdgesCount === 4) {
+        this.boxes[edgeBox.id].complete = true;
+        this.boxes[edgeBox.id].player = this.players.current;
+        this.players.scores[this.players.current] += 1;
+        completedBoxes++;
       }
+    }
+    if (completedBoxes > 0) {
+      console.log(
+        `Player ${this.players.current} completed ${completedBoxes} box(es) and gets another move.`
+      );
+    } else {
+      this.players.current = this.players.current === 0 ? 1 : 0;
     }
   }
 
   makeMove(move) {
     this.setEdge(move);
-    // After making the move, check if it completes any boxes for the current player
-    const completedBoxes = this.findAdjacentBoxes(move).filter((box) =>
-      this.isBoxComplete(box)
-    );
-
-    if (completedBoxes.length > 0) {
-      // If the move completed any boxes, award points to the current player
-      this.players.scores[this.players.current] += completedBoxes.length;
-    } else {
-      // Switch turns if no boxes are completed
-      this.players.current = this.players.current === 0 ? 1 : 0;
+    if (this.players.current === 0) {
+      this.playBestMove();
     }
   }
 
+  // findBestMove() {
+  //   let bestMove = null;
+  //   let minRisk = Infinity;
+
+  //   // Step 1: Prioritize completing boxes
+  //   for (let key in this.edges) {
+  //     let edge = this.edges[key];
+
+  //     if (!edge.drawn) {
+  //       const adjacentBoxes = this.findAdjacentBoxes(edge);
+
+  //       // If the move completes a box, prioritize it
+  //       if (adjacentBoxes.some((box) => this.isBoxOneMoveFromCompletion(box))) {
+  //         return edge;
+  //       }
+  //     }
+  //   }
+
+  //   // Step 2: Find the least risky move (that does not give the opponent an easy win)
+  //   for (let key in this.edges) {
+  //     let edge = this.edges[key];
+
+  //     if (!edge.drawn) {
+  //       const adjacentBoxes = this.findAdjacentBoxes(edge);
+  //       let risk = this.calculateMoveRisk(
+  //         adjacentBoxes,
+  //         /* checkOpponentRisk= */ true
+  //       );
+
+  //       // Keep track of the move with the lowest risk
+  //       if (risk < minRisk) {
+  //         minRisk = risk;
+  //         bestMove = edge;
+  //       }
+  //     }
+  //   }
+
+  //   // Step 3: If no safe move, sacrifice two squares (if necessary)
+  //   if (!bestMove) {
+  //     // Find a move that sacrifices two boxes, but minimizes further risk
+  //     let leastBadMove = null;
+  //     let leastBadRisk = Infinity;
+
+  //     for (let key in this.edges) {
+  //       let edge = this.edges[key];
+
+  //       if (!edge.drawn) {
+  //         const adjacentBoxes = this.findAdjacentBoxes(edge);
+  //         let risk = this.calculateMoveRisk(
+  //           adjacentBoxes,
+  //           /* checkOpponentRisk= */ false
+  //         );
+
+  //         // Choose the move that results in sacrificing two boxes, but with the lowest overall risk
+  //         if (risk < leastBadRisk) {
+  //           leastBadRisk = risk;
+  //           leastBadMove = edge;
+  //         }
+  //       }
+  //     }
+
+  //     return leastBadMove; // This sacrifices two squares
+  //   }
+
+  //   return bestMove;
+  // }
+
   findBestMove() {
+    //   let bestMove = null;
+    // let maxScore = -Infinity;
+
+    // for (let key in this.edges) {
+    //   let edge = this.edges[key];
+    //   if (edge.drawn) continue;
+
+    //   // Check if drawing this edge completes a box or creates a risky situation
+    //   let boxes = this.findAdjacentBoxes(edge);
+    //   let completedBoxes = boxes.filter((box) => this.isBoxOneMoveFromCompletion(box));
+    //   let riskyMove = completedBoxes.length > 0 || this.isSacrificeNecessary(boxes);
+
+    //   if (!riskyMove) {
+    //     // Prioritize non-risky moves
+    //     let score = this.calculateMoveScore(boxes);
+    //     if (score > maxScore) {
+    //       maxScore = score;
+    //       bestMove = edge;
+    //     }
+    //   }
+    // }
+
+    // // If no safe moves are found, sacrifice two squares
+    // if (!bestMove) {
+    //   bestMove = this.findSacrificeMove();
+    // }
+
+    // return bestMove;
     let bestMove = null;
+    let maxScore = -Infinity;
     let minRisk = Infinity;
 
-    // Step 1: Prioritize completing boxes
     for (let key in this.edges) {
       let edge = this.edges[key];
+      if (edge.drawn) continue;
 
-      if (!edge.drawn) {
-        const adjacentBoxes = this.findAdjacentBoxes(edge);
+      // Check if drawing this edge completes a box
+      let boxes = this.findAdjacentBoxes(edge);
+      let completedBoxes = boxes.filter(
+        (box) => box.drawnEdgesCount === 3
+        // this.isBoxOneMoveFromCompletion(box)
+      );
 
-        // If the move completes a box, prioritize it
-        if (adjacentBoxes.some((box) => this.isBoxOneMoveFromCompletion(box))) {
-          return edge;
-        }
+      if (completedBoxes.length > 0) {
+        // Prioritize completing a box, return immediately
+        return edge;
+      }
+
+      // Evaluate risk of move
+      let risk = this.calculateMoveRisk(boxes);
+
+      // Calculate score based on risk and other factors
+      let score = this.calculateMoveScore(boxes);
+
+      // Update best move if necessary
+      if (score > maxScore || (score === maxScore && risk < minRisk)) {
+        maxScore = score;
+        minRisk = risk;
+        bestMove = edge;
       }
     }
 
-    // Step 2: Find the least risky move (that does not give the opponent an easy win)
-    for (let key in this.edges) {
-      let edge = this.edges[key];
-
-      if (!edge.drawn) {
-        const adjacentBoxes = this.findAdjacentBoxes(edge);
-        let risk = this.calculateMoveRisk(
-          adjacentBoxes,
-          /* checkOpponentRisk= */ true
-        );
-
-        // Keep track of the move with the lowest risk
-        if (risk < minRisk) {
-          minRisk = risk;
-          bestMove = edge;
-        }
+    // Check if best move is too risky
+    if (minRisk > 1) {
+      // Consider sacrificial moves
+      let sacrificialMove = this.findSacrificialMove();
+      if (sacrificialMove) {
+        return sacrificialMove;
       }
-    }
-
-    // Step 3: If no safe move, sacrifice two squares (if necessary)
-    if (!bestMove) {
-      // Find a move that sacrifices two boxes, but minimizes further risk
-      let leastBadMove = null;
-      let leastBadRisk = Infinity;
-
-      for (let key in this.edges) {
-        let edge = this.edges[key];
-
-        if (!edge.drawn) {
-          const adjacentBoxes = this.findAdjacentBoxes(edge);
-          let risk = this.calculateMoveRisk(
-            adjacentBoxes,
-            /* checkOpponentRisk= */ false
-          );
-
-          // Choose the move that results in sacrificing two boxes, but with the lowest overall risk
-          if (risk < leastBadRisk) {
-            leastBadRisk = risk;
-            leastBadMove = edge;
-          }
-        }
-      }
-
-      return leastBadMove; // This sacrifices two squares
     }
 
     return bestMove;
   }
 
+  calculateMoveScore(boxes) {
+    let score = 0;
+    boxes.forEach((box) => {
+      let drawnEdges = Object.values(box.edges).filter((e) => e.drawn).length;
+      if (drawnEdges === 2) {
+        // Encourage moves that leave the opponent with 2 sides completed
+        score += 2;
+      } else if (drawnEdges === 1) {
+        // Slightly discourage creating opportunities for opponent with 1 side
+        score -= 1;
+      } else if (drawnEdges === 3 && !box.complete) {
+        // Discourage moves that give the opponent a box with 3 sides
+        score -= 5;
+      }
+    });
+    return score;
+    // let score = 0;
+    // boxes.forEach((box) => {
+    //   let drawnEdges = Object.values(box.edges).filter((e) => e.drawn).length;
+    //   if (drawnEdges === 2) {
+    //     // Encourage moves that leave the opponent with 2 sides completed
+    //     score += 2;
+    //   } else if (drawnEdges === 1) {
+    //     // Slightly discourage creating opportunities for opponent with 1 side
+    //     score -= 1;
+    //   }
+    // });
+    // return score;
+  }
+
+  calculateMoveRisk(boxes) {
+    let risk = 0;
+    boxes.forEach((box) => {
+      let drawnEdges = Object.values(box.edges).filter((e) => e.drawn).length;
+      if (drawnEdges === 2) {
+        risk += 2; // Higher risk if opponent can complete in one move
+      } else if (drawnEdges === 1) {
+        risk += 1; // Lower risk if opponent can complete in two moves
+      }
+    });
+    return risk;
+  }
+
+  isSacrificeNecessary(boxes) {
+    for (let box of boxes) {
+      if (this.isBoxOneMoveFromCompletion(box)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  findSacrificialMove() {
+    for (let key in this.edges) {
+      let edge = this.edges[key];
+      if (edge.drawn) continue;
+
+      let boxes = this.findAdjacentBoxes(edge);
+      if (this.isSacrificeNecessary(boxes)) {
+        return edge;
+      }
+    }
+    return null;
+  }
+
   findAdjacentBoxes(edge) {
     // Find all boxes adjacent to the given edge
+    //  let edgeBoxes = this.boxes.filter(
+    //   (b, i) =>
+    //     !b.complete &&
+    //     b.edges[edge.rowColKey] &&
+    //     !b.edges[edge.rowColKey].drawn &&
+    //     b.edges[edge.rowColKey].boxIndex === i
+    // ),
     return this.boxes.filter((box) =>
       Object.values(box.edges).some(
         (boxEdge) => boxEdge.rowColKey === edge.rowColKey
@@ -344,21 +496,21 @@ export class GameState {
     return Object.values(box.edges).every((edge) => edge.drawn);
   }
 
-  calculateMoveRisk(adjacentBoxes, checkOpponentRisk = false) {
-    let risk = 0;
+  // calculateMoveRisk(adjacentBoxes, checkOpponentRisk = false) {
+  //   let risk = 0;
 
-    adjacentBoxes.forEach((box) => {
-      if (this.isBoxOneMoveFromCompletion(box)) {
-        risk += 1;
-      }
+  //   adjacentBoxes.forEach((box) => {
+  //     if (this.isBoxOneMoveFromCompletion(box)) {
+  //       risk += 1;
+  //     }
 
-      if (checkOpponentRisk && this.willGiveOpponentBox(box)) {
-        risk += 2; // Higher risk if this move gives the opponent a box
-      }
-    });
+  //     if (checkOpponentRisk && this.willGiveOpponentBox(box)) {
+  //       risk += 2; // Higher risk if this move gives the opponent a box
+  //     }
+  //   });
 
-    return risk; // Lower risk is better
-  }
+  //   return risk; // Lower risk is better
+  // }
 
   willGiveOpponentBox(box) {
     // Check if the opponent will complete a box after this move
@@ -372,6 +524,7 @@ export class GameState {
     let bestMove = this.findBestMove();
     if (bestMove) {
       this.makeMove(bestMove);
+      // console.log(this.players);
     }
   }
 }
--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -4,29 +4,64 @@
   let canvas,
     ctx,
     gap = 60,
-    size = 3,
+    size = 4,
     initialVertex = { x: 50, y: 50 },
-    state = new GameState(initialVertex, size, gap);
+    state = new GameState(initialVertex, size, gap),
+    edges = Object.values(state.edges);
 
   state.generateVertices();
   state.generateBoxes();
+  console.log(state.boxes);
   //   state.getAvailableMoves();
   const draw = () => {
-      for (let i = 0; i < Object.values(state.edges).length; i++) {
-        let { from, to, drawn } = Object.values(state.edges)[i];
-        ctx.beginPath();
-        ctx.arc(from.x, from.y, 5, 0, 2 * Math.PI);
-        ctx.arc(to.x, to.y, 5, 0, 2 * Math.PI);
-        ctx.closePath();
-        ctx.fill();
-        if (drawn) {
+      for (let box of state.boxes) {
+        for (let edge of Object.values(box.edges)) {
+          let { from, to, drawn, c } = edge,
+            textCenter = {
+              x: (from.x + to.x) / 2,
+              y: (from.y + to.y) / 2,
+            };
+          ctx.fillStyle = "black";
           ctx.beginPath();
-          ctx.moveTo(from.x, from.y);
-          ctx.lineTo(to.x, to.y);
+          ctx.arc(from.x, from.y, 5, 0, 2 * Math.PI);
+          ctx.arc(to.x, to.y, 5, 0, 2 * Math.PI);
           ctx.closePath();
-          ctx.stroke();
+          ctx.fill();
+          if (box.complete) {
+            ctx.fillStyle = "black";
+            ctx.font = "30px serif";
+            ctx.textAlign = "center";
+            ctx.fillText(box.player, textCenter.x, textCenter.y);
+          }
+          if (drawn) {
+            ctx.fillStyle = c;
+            ctx.strokeStyle = c;
+            ctx.beginPath();
+            ctx.moveTo(from.x, from.y);
+            ctx.lineTo(to.x, to.y);
+            ctx.closePath();
+            ctx.stroke();
+          }
         }
       }
+      // for (let i = 0; i < Object.values(state.edges).length; i++) {
+      //   let { from, to, drawn, c } = Object.values(state.edges)[i];
+      // ctx.fillStyle = "black";
+      // ctx.beginPath();
+      // ctx.arc(from.x, from.y, 5, 0, 2 * Math.PI);
+      // ctx.arc(to.x, to.y, 5, 0, 2 * Math.PI);
+      // ctx.closePath();
+      // ctx.fill();
+      // if (drawn) {
+      //   ctx.fillStyle = c;
+      //   ctx.strokeStyle = c;
+      //   ctx.beginPath();
+      //   ctx.moveTo(from.x, from.y);
+      //   ctx.lineTo(to.x, to.y);
+      //   ctx.closePath();
+      //   ctx.stroke();
+      // }
+      // }
     },
     makeMove = () => {
       state.playBestMove();
@@ -34,9 +69,28 @@
     };
   onMount(() => {
     ctx = canvas.getContext("2d");
+    edges = Object.values(state.edges);
     draw();
   });
 </script>
 
-<button on:click={makeMove}>make move</button>
-<canvas width="1280" height="720" bind:this={canvas}></canvas>
+<div style="width: 300px; display:flex; flex-direction:column; gap: 10px">
+  <button on:click={makeMove}>make move</button>
+  {#each edges as edge, index}
+    {#if !edge.drawn}
+      <button
+        on:click={() => {
+          state.makeMove(edge);
+          edges = Object.values(state.edges);
+          draw();
+        }}
+      >
+        {edge.rowColKey} - {index}
+        <!-- {state.edges[key].drawn} -->
+      </button>
+    {/if}
+  {/each}
+</div>
+<div>
+  <canvas width="1280" height="720" bind:this={canvas}></canvas>
+</div>
